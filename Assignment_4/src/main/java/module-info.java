53ab39c53fa5da2f08afbdbe64483d213b9b2157
owenwongny <ow321@connect.hku.hk>
2023-03-31 17:25:59 +0000

    fixing dependencies

--- Assignment_4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignment_4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.assignment_4 to javafx.fxml;
-    exports com.example.assignment_4;
-}
